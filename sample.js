75a54921b2ede3b8d880e11a056cf83ac28d00b9
mjv761 <michael.vanderkamp@usask.ca>
2019-02-21 11:38:11 -0600

    Add Swipe demo, adjust colours, clarify 'all'

--- sample.js
+++ sample.js
@@ -3,8 +3,18 @@
 const region = new westures.Region(window);
 const container = document.querySelector('#container');
 
+const SIXTY_FPS = 1000 / 60;
+const FRICTION = 0.95;
+const MULTI = 2;
+const LIMIT = 0.1;
+
 function random8Bit() {
   return Math.floor(Math.random() * 256);
+}
+
+function reduce(value) {
+  value *= FRICTION;
+  return (Math.abs(value) < LIMIT) ? 0 : value;
 }
 
 class Interactable {
@@ -21,8 +31,21 @@ class Interactable {
     this.scale = 1;
     this.x = 0;
     this.y = 0;
+    this.velocityX = 0;
+    this.velocityY = 0;
 
-    setInterval(() => this.update(), 50 / 3);
+    this.swipe_interval = null;
+    this.animate_swipe = () => {
+      this.x += this.velocityX;
+      this.y += this.velocityY;
+      this.velocityX = reduce(this.velocityX);
+      this.velocityY = reduce(this.velocityY);
+      if (this.velocityY === 0 && this.velocityX === 0) {
+        clearInterval(this.swipe_interval);
+      }
+    };
+
+    setInterval(() => this.update(), SIXTY_FPS);
   }
 
   addTap() {
@@ -48,6 +71,23 @@ class Interactable {
       (data) => {
         this.x += data.change.x;
         this.y += data.change.y;
+      });
+    return this;
+  }
+
+  addSwipe() {
+    region.addGesture(this.element,
+      new westures.Swipe(),
+      (data) => {
+        this.rotation = data.direction;
+        this.velocityX = data.velocity * Math.cos(data.direction) * MULTI;
+        this.velocityY = data.velocity * Math.sin(data.direction) * MULTI;
+        this.swipe_interval = setInterval(this.animate_swipe, SIXTY_FPS); 
+      });
+    region.addGesture(this.element,
+      new westures.Track(['start']),
+      (data) => {
+        clearInterval(this.swipe_interval);
       });
     return this;
   }
@@ -81,12 +121,15 @@ class Interactable {
 }
 
 new Interactable('TAP',    'crimson').addTap();
-new Interactable('SWIVEL', 'orange').addSwivel(false);
-new Interactable('PAN',    'yellow').addPan();
-new Interactable('PINCH',  'green' ).addPinch();
-new Interactable('ROTATE', 'blue'  ).addRotate();
-new Interactable('ALL',    'purple')
-  .addTap()
+new Interactable('SWIVEL', 'darkorange').addSwivel(false);
+new Interactable('PAN',    'gold').addPan();
+new Interactable('PINCH',  'green').addPinch();
+new Interactable('ROTATE', 'dodgerblue').addRotate();
+new Interactable('SWIPE',  'darkorchid').addSwipe();
+new Interactable(
+  'TAP, PAN, PINCH, and ROTATE\n(desktop: CTRL to SWIVEL)', 
+  'yellowgreen'
+).addTap()
   .addPan()
   .addPinch()
   .addRotate()
